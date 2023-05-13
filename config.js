3891c4a559bcd7c5f7b0022f273e759d3df40bc3
YuvalTzoor <yuval1abc@gmail.com>
2023-05-13 17:34:36 +0300

    test

--- config.js
+++ config.js
@@ -12,5 +12,4 @@ const CONFIG = {
 module.exports = {
 	CONFIG,
 };
-//URL = 'mongodb:localhost:27017/quotes'
-// URL: "mongodb+srv://Demo_User_1:JgrTW1umUdCqXHtw@meantestdb.rsdpijy.mongodb.net/Demo_User_1_DB",
+
