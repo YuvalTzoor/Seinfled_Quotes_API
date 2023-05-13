a421ed26c67173c37a76dc40345652ca7b2ed00e
YuvalTzoor <yuval1abc@gmail.com>
2023-05-13 16:38:48 +0300

    test

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 // config.js
 
 const CONFIG = {
-	URL: "mongodb+srv://Demo_User_1:JgrTW1umUdCqXHtw@meantestdb.rsdpijy.mongodb.net/Demo_User_1_DB",
+	URL: "mongodb:localhost:27017/quotes",
 	// Replace "database-name" with your desired database name
 };
 
 module.exports = {
 	CONFIG,
 };
-//mongodb+srv://Admin_YT:KVt3qekXYSLcMpiC@meantestdb.rsdpijy.mongodb.net/?retryWrites=true&w=majority
-//mongodb+srv://Demo_User_1:JgrTW1umUdCqXHtw@meantestdb.rsdpijy.mongodb.net/Demo_User_1_DB
+//URL = 'mongodb:localhost:27017/quotes'
+// URL: "mongodb+srv://Demo_User_1:JgrTW1umUdCqXHtw@meantestdb.rsdpijy.mongodb.net/Demo_User_1_DB",
