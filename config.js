e5e1fba510362e2969afae14a5cdafaeee4c3385
YuvalTzoor <yuval1abc@gmail.com>
2023-05-13 17:17:37 +0300

    test

--- config.js
+++ config.js
@@ -1,7 +1,11 @@
-// config.js
+//will add the process.env.MONGODB_URL to the config.js file
+// Path: config.js
+// Compare this snippet from app.js:
+
+var process = require("process");
 
 const CONFIG = {
-	URL: "mongodb+srv://Demo_User_1:JgrTW1umUdCqXHtw@meantestdb.rsdpijy.mongodb.net/Demo_User_1_DB",
+	URL: process.env.MONGODB_URL,
 	// Replace "database-name" with your desired database name
 };
 
