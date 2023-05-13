f2280afc121935ad43ce689104928a13b15958c8
YuvalTzoor <yuval1abc@gmail.com>
2023-05-13 16:32:23 +0300

    test

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 // config.js
 
 const CONFIG = {
-	// URL: "mongodb+srv://Demo_User_1:JgrTW1umUdCqXHtw@meantestdb.rsdpijy.mongodb.net/Demo_User_1_DB",
-	URL: "mongodb://localhost:27017/seinfeld_quotes", // Replace "database-name" with your desired database name
+	URL: "mongodb+srv://Demo_User_1:JgrTW1umUdCqXHtw@meantestdb.rsdpijy.mongodb.net/Demo_User_1_DB",
+	// Replace "database-name" with your desired database name
 };
 
 module.exports = {
