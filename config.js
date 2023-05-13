fc88c4c608323f017e63c7a1aeaa6e8a50706d0b
YuvalTzoor <yuval1abc@gmail.com>
2023-05-13 16:43:19 +0300

    test

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // config.js
 
 const CONFIG = {
-	URL: "mongodb://localhost:27017/quotes",
+	URL: "mongodb+srv://Demo_User_1:JgrTW1umUdCqXHtw@meantestdb.rsdpijy.mongodb.net/Demo_User_1_DB",
 	// Replace "database-name" with your desired database name
 };
 
